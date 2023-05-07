1ddc491aa0395d00edac022593961a8019bd712c
Mohana <mpkulkarni117@gmail.com>
2023-05-07 11:37:06 +0530

    Added Income Section

--- components/transactions/addNewTransaction.js
+++ components/transactions/addNewTransaction.js
@@ -1,187 +1,206 @@
-import { Row } from "@nextui-org/react"
-import { useState, useMemo } from "react"
+import { Row } from "@nextui-org/react";
+import { useState, useMemo } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from "./addNewTransaction.module.css";
 import { Input, Dropdown, css } from "@nextui-org/react";
-import { Textarea  } from '@nextui-org/react';
-import { Button } from '@nextui-org/react';
+import { Textarea } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { GetTransactions } from "../constants/urls";
 
 export const AddNewTransaction = (props) => {
-    const [selectedCat, setSelectedCat] = useState(new Set(["Choose a category"]));
-    const [transactionName, setTransactionName] = useState("");
-    const [transactionAmount, setTransactionAmount] = useState(0);
-    const [transactionDescription, setTransactionDescription] = useState("");
+  const [selectedCat, setSelectedCat] = useState(
+    new Set(["Choose a category"])
+  );
+  const [transactionName, setTransactionName] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState(0);
+  const [transactionDescription, setTransactionDescription] = useState("");
+  const [addIncome, setAddIncome] = useState(false);
+  const selectedValue = useMemo(
+    () => Array.from(selectedCat).join(", ").replaceAll("_", " "),
+    [selectedCat]
+  );
 
-    const selectedValue = useMemo(
-        () => Array.from(selectedCat).join(", ").replaceAll("_", " "),
-        [selectedCat]
-      );
+  const dropdownStyle = {
+    backgroundColor: "#2B2F31",
+  };
 
-    const dropdownStyle = {
-        backgroundColor: '#2B2F31',
+  const inputStyle = {
+    // color: '#$pink100',
+    backgroundColor: "white",
+    width: "100%",
+  };
+
+  const textAreaStyle = {
+    width: "100%",
+  };
+
+  const addButtonStyle = {
+    width: "50%",
+  };
+
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = currentDate.getDay();
+    if (currentMonth < 10) {
+      currentMonth = "0" + String(currentMonth);
     }
-
-    const inputStyle = {
-        // color: '#$pink100',
-        backgroundColor: 'white',
-        width: '100%',
+    if (currentDay < 10) {
+      currentDay = "0" + String(currentDay);
     }
+    let formattedDate = `${currentYear}-${currentMonth}-${currentDay}`;
+    console.log(formattedDate);
+    return formattedDate;
+  };
 
-    const textAreaStyle = {
-        width: '100%',
+  const getCategoryIdFromCategory = (category) => {
+    let categoryName = Array.from(category)[0];
+    console.log(categoryName);
+    for (let i = 0; i < props.catData.length; i++) {
+      if (categoryName === props.catData[i]["name"]) {
+        return props.catData[i]["id"];
+      }
     }
+  };
 
-    const addButtonStyle = {
-        width: '50%',
-    }
+  const handleTransaction = async () => {
+    const transactionDate = getCurrentDate();
+    const categoryId = getCategoryIdFromCategory(selectedCat);
 
-    const getCurrentDate = () => {
-        const currentDate = new Date();
-        let currentYear = currentDate.getFullYear();
-        let currentMonth = currentDate.getMonth() + 1;
-        let currentDay = currentDate.getDay();
-        if (currentMonth < 10) {
-            currentMonth = "0" + String(currentMonth);
-        }
-        if (currentDay < 10) {
-            currentDay = "0" + String(currentDay);
-        }
-        let formattedDate = `${currentYear}-${currentMonth}-${currentDay}`;
-        console.log(formattedDate);
-        return formattedDate;
-    }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Cookie",
+      "ARRAffinity=ab52746cee3d2041a0a4d964700673b28428853c105b7c164f6460eb8129960d; ARRAffinitySameSite=ab52746cee3d2041a0a4d964700673b28428853c105b7c164f6460eb8129960d"
+    );
 
-    const getCategoryIdFromCategory = (category) => {
-        let categoryName = Array.from(category)[0];
-        console.log(categoryName);
-        for (let i=0; i<props.catData.length; i++) {
-            if (categoryName === props.catData[i]['name']) {
-                return(props.catData[i]['id']);
-            }
-        }
-    }
+    var raw = JSON.stringify({
+      name: `${transactionName}`,
+      description: `${transactionDescription}`,
+      amount: Number(transactionAmount),
+      date: `${transactionDate}`,
+      transactionTypeId: addIncome ? 2 : 1,
+      categoryId: addIncome ? 7 : categoryId,
+    });
 
-    const handleTransaction = async () => {
-        const transactionDate = getCurrentDate();
-        const categoryId = getCategoryIdFromCategory(selectedCat);
+    console.log(raw);
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Cookie", "ARRAffinity=ab52746cee3d2041a0a4d964700673b28428853c105b7c164f6460eb8129960d; ARRAffinitySameSite=ab52746cee3d2041a0a4d964700673b28428853c105b7c164f6460eb8129960d");
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
 
-        var raw = JSON.stringify({
-            "name": `${transactionName}`,
-            "description": `${transactionDescription}`,
-            "amount": Number(transactionAmount),
-            "date": `${transactionDate}`,
-            "transactionTypeId": 1,
-            "categoryId": categoryId,
-        });
+    let response = await fetch(`${GetTransactions}`, requestOptions);
 
-        console.log(raw);
-        
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
+    let responseData = await response.json();
+    console.log(responseData);
+  };
 
-        let response = await fetch(
-            `${GetTransactions}`,
-            requestOptions
-        )
-
-        let responseData = await response.json()
-        console.log(responseData);
-    }
-
-    return (
-        <div className={styles.mainContainer}>
-            <h1>Create a new Transaction</h1>
-            <Row justify="center" className={styles.containerRow}>
-                <div className={styles.newTranContainer}>
-                    {/* <div>
+  return (
+    <div className={styles.mainContainer}>
+      <h1>Create a new Transaction</h1>
+      <Button.Group>
+        <Button
+          onPress={() => {
+            setAddIncome(false);
+          }}
+        >
+          Expense
+        </Button>
+        <Button
+          onPress={() => {
+            setAddIncome(true);
+          }}
+        >
+          Income
+        </Button>
+      </Button.Group>
+      <Row justify="center" className={styles.containerRow}>
+        <div className={styles.newTranContainer}>
+          {/* <div>
                         <p>Date</p>
                         <DatePicker className={styles.inputDate} selected={transDate} onChange={(date) => setTransDate(date)} />
                     </div> */}
-                    <div>
-                        <p>Name</p>
-                        <Input 
-                            // color="primary"
-                            bordered
-                            placeholder="Enter Name" 
-                            css={inputStyle}
-                            onChange={(event) => {
-                                setTransactionName(event.target.value);
-                            }}
-                            value={transactionName}
-                        />
-                    </div>
-                    <div className={styles.dropdownDiv}>
-                        <p>Category</p>
-                        <Dropdown>
-                          <Dropdown.Button
-                            solid
-                            color='primary'
-                          >
-                            {selectedCat}
-                          </Dropdown.Button>
-                          <Dropdown.Menu 
-                            aria-label="Static Actions"
-                            color='primary'
-                            css={dropdownStyle}
-                            disallowEmptySelection
-                            selectionMode="single"
-                            selectedKeys={selectedCat}
-                            onSelectionChange={setSelectedCat}
-                          >
-                          {props.catData.map((cat) => {
-                            return(
-                                <Dropdown.Item color="primary" key={cat['name']}>{cat['name']}</Dropdown.Item>
-                            )
-                          })}
-                          </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                    <div>
-                        <p>Amount</p>
-                        <Input 
-                            // color="primary"
-                            bordered
-                            placeholder="Enter amount" 
-                            css={inputStyle}
-                            onChange={(event) => {
-                                setTransactionAmount(event.target.value)
-                            }}
-                        />
-                    </div>
-                    
-                    <div>
-                        <p>Description (Optional)</p>
-                        <Textarea
-                            placeholder="Enter Description"
-                            css={textAreaStyle}
-                            onChange={(event) => {
-                                setTransactionDescription(event.target.value)
-                            }}
-                        />
-                    </div>
-                    <div className={styles.addButtonDiv}>
-                        <Button 
-                            color="success" 
-                            auto 
-                            onPress={handleTransaction}
-                            css={addButtonStyle}
-                        >
-                            Add Transaction
-                        </Button>
-                    </div>
-                </div>
-            </Row>
-        </div>
-    )
-}
+          <div>
+            <p>Name</p>
+            <Input
+              // color="primary"
+              bordered
+              placeholder="Enter Name"
+              css={inputStyle}
+              onChange={(event) => {
+                setTransactionName(event.target.value);
+              }}
+              value={transactionName}
+            />
+          </div>
+          {!addIncome && (
+            <div className={styles.dropdownDiv}>
+              <p>Category</p>
+              <Dropdown>
+                <Dropdown.Button solid color="primary">
+                  {selectedCat}
+                </Dropdown.Button>
+                <Dropdown.Menu
+                  aria-label="Static Actions"
+                  color="primary"
+                  css={dropdownStyle}
+                  disallowEmptySelection
+                  selectionMode="single"
+                  selectedKeys={selectedCat}
+                  onSelectionChange={setSelectedCat}
+                >
+                  {props.catData.map((cat) => {
+                    return (
+                      <Dropdown.Item color="primary" key={cat["name"]}>
+                        {cat["name"]}
+                      </Dropdown.Item>
+                    );
+                  })}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          )}
 
+          <div>
+            <p>Amount</p>
+            <Input
+              // color="primary"
+              bordered
+              placeholder="Enter amount"
+              css={inputStyle}
+              onChange={(event) => {
+                setTransactionAmount(event.target.value);
+              }}
+            />
+          </div>
+
+          <div>
+            <p>Description (Optional)</p>
+            <Textarea
+              placeholder="Enter Description"
+              css={textAreaStyle}
+              onChange={(event) => {
+                setTransactionDescription(event.target.value);
+              }}
+            />
+          </div>
+          <div className={styles.addButtonDiv}>
+            <Button
+              color="success"
+              auto
+              onPress={handleTransaction}
+              css={addButtonStyle}
+            >
+              Add Transaction
+            </Button>
+          </div>
+        </div>
+      </Row>
+    </div>
+  );
+};
--- components/transactions/transTable.js
+++ components/transactions/transTable.js
@@ -1,53 +1,55 @@
-import { Table, Row } from '@nextui-org/react';
+import { Table, Row } from "@nextui-org/react";
 
 export const TransTable = (props) => {
-    const headerStyle = {
-        backgroundColor: '#2B2F31',
-        color: '#9BA1A6',
-    };
-    const rowStyle = {
-        color: 'white',
-    };
-  
-    return (
-        <div>
-          {/* <h1>Hello</h1> */}
-          <Row justify='center'>
-            <Table
-                aria-label="Example table with static content"
-                    css={{
-                      height: "auto",
-                      th: headerStyle,
-                      tr: rowStyle,
-                    }}
-            >
-              <Table.Header>
-                <Table.Column>CATEGORY</Table.Column>
-                <Table.Column>DATE</Table.Column>
-                <Table.Column>NAME</Table.Column>
-                <Table.Column>DESCRIPTION</Table.Column>
-              </Table.Header>
-              <Table.Body>
-                {props.transData.map((data) => {
-                  return(
-                      <Table.Row key={data.id}>
-                        <Table.Cell>{props.catData[data.categoryId - 1]['name']}</Table.Cell>
+  const headerStyle = {
+    backgroundColor: "#2B2F31",
+    color: "#9BA1A6",
+  };
+  const rowStyle = {
+    color: "white",
+  };
 
-                        
-                        <Table.Cell>{data.date}</Table.Cell>
-                        <Table.Cell>{data.name}</Table.Cell>
-                        {data.description ? 
-                            (<Table.Cell>{data.description}</Table.Cell>)
-                            :
-                            (<Table.Cell>NA</Table.Cell>)
-                        }
-                        
-                      </Table.Row>
-                  )
-                })}
-              </Table.Body>
-            </Table>
-          </Row>
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* <h1>Hello</h1> */}
+      <Row justify="center">
+        <Table
+          aria-label="Example table with static content"
+          css={{
+            height: "auto",
+            th: headerStyle,
+            tr: rowStyle,
+          }}
+        >
+          <Table.Header>
+            <Table.Column>NAME</Table.Column>
+            <Table.Column>AMOUNT</Table.Column>
+            <Table.Column>CATEGORY</Table.Column>
+            <Table.Column>DATE</Table.Column>
+            <Table.Column>DESCRIPTION</Table.Column>
+          </Table.Header>
+          <Table.Body>
+            {props.transData.map((data) => {
+              return (
+                <Table.Row key={data.id}>
+                  <Table.Cell>{data.name}</Table.Cell>
+                  <Table.Cell>{data.amount}</Table.Cell>
+                  <Table.Cell>
+                    {props.catData[data.categoryId - 1]["name"]}
+                  </Table.Cell>
+
+                  <Table.Cell>{data.date}</Table.Cell>
+                  {data.description ? (
+                    <Table.Cell>{data.description}</Table.Cell>
+                  ) : (
+                    <Table.Cell>NA</Table.Cell>
+                  )}
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </Row>
+    </div>
+  );
+};
